c4b75fe5ddf53dbc7a5d1462b5760f0dfb80acf3
shanzi <yun.er.run@gmail.com>
2011-10-09 23:50:21 +0800

    FmView，继承于WebView，用以完成豆瓣音乐的主要功能

--- FmView.h
+++ FmView.h
@@ -1,5 +1,5 @@
 //
-//  FmController.h
+//  FmView.h
 //  dmu
 //
 //  Created by Shanzi on 11-10-9.
@@ -7,11 +7,16 @@
 //
 
 #import <Foundation/Foundation.h>
-#import <Foundation/NSArray.h>
-#import <Foundation/NSString.h>
+#import <WebKit/WebView.h>
+#import "FmController.h"
 
-@protocol FmController <NSObject>
-// 基本控制方法
+@interface FmView : WebView <FmController>
+{
+    NSMutableDictionary* config;
+}
+
+-(id) initWithConfig:(NSDictionary*) config;
+
 -(NSArray*) play; //开始播放音乐，返回当前音乐的信息
 -(void) pause;
 -(void) next;
@@ -21,10 +26,9 @@
 //获取当前播放中的歌曲信息
 -(void) currentMusicData;
 
+
 //与webkit里的JavaScript交互的方法
 -(void) receiveJavaScriptSignal; // 接收到JavaScript发出的消息的控制函数
 
 
-@optional
--(void) back;
 @end
