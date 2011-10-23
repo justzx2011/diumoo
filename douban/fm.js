088b315a5dcf52a845d9347b6a2320d3b5e3d8b1
shanzi <yun.er.run@gmail.com>
2011-10-23 23:24:43 +0800

    成功模拟认证功能，获取了对应的cookie和信息并保存

--- douban/fm.js
+++ douban/fm.js
@@ -2,7 +2,7 @@
 var source="radio"
 var appname="dmu";
 var appversion="1";
-
+var MAX_RETRY_TIME=5;
 
 //全局对象
 var playlist=[];
@@ -12,39 +12,35 @@ var auth_obj={};
 
 
 
-/**
- * function intro
- * @param {type} email,pass,appname,version
- * @return 
- */
-function auth(email,pass) {
-    $.post('http://douban.fm/j/login',
+function auth(email,pass,retry) {
+    $.ajax('http://douban.fm/j/login',
             {
-                'alias':email,
-                'form_password':pass,
-                'source':appname,
-            },
-            function(data){
-                try{
-                    var tmp=eval(data,xhr);
-                    if(typeof(tmp)!="undefined" && !tmp.r){
+                'data':{
+                    'alias':email,
+                    'form_password':pass,
+                    'source':appname,
+                },
+                'type':'post',
+                'beforeSend':function(xhr,settings){
+                    xhr.setRequestHeader('cookie','');
+                },
+                'success':function(data,sta,xhr){
+                    try{
+                        var tmp=eval(data);
+                        if(typeof(tmp)!="undefined" && !tmp.r){
                             auth_obj=tmp;
-                            cookie=xhr.getResponseHeader('Set-Cookie');
+                            auth_obj.cookie=xhr.getResponseHeader('Set-Cookie').match(/expires=.+?,\s*(\w+=".+?");/g).join('').replace(/expires=(.+?,){2}/g,'');
+                        }
+                        else throw(0,"Authenticated Failed ("+tmp.err+")");
+                    }catch(ex){
+                        throw(1,"Connection Error");
                     }
-                    else throw(0,"Authenticated Failed ("+tmp.err+")");
-
-                }catch(ex){
-                    error_handle(ex);
                 }
+            
             });
 }
 
 
-/**
- * function intro
- * @param {type} err
- * @return 
- */
 function error_handle(err) {
     throw err
 }
