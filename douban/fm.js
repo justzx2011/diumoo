2fc89f3f7b063b07631566a1a5948976b32f19cc
shanzi <yun.er.run@gmail.com>
2011-10-25 21:08:40 +0800

    成功获得随机的新播放列表

--- douban/fm.js
+++ douban/fm.js
@@ -3,6 +3,8 @@ var source="radio"
 var appname="dmu";
 var appversion="1";
 var MAX_RETRY_TIME=5;
+var MIN_R=Math.pow(16,9);
+var RANGE_R=Math.pow(16,10)-MIN_R-1;
 
 //全局对象
 var playlist=[];
@@ -33,12 +35,40 @@ function auth(email,pass,retry) {
                         }
                         else throw(0,"Authenticated Failed ("+tmp.err+")");
                     }catch(ex){
-                        throw(1,"Connection Error");
+                        throw(1,"Returned Invalid Data");
                     }
                 }
             
             });
 }
+
+function get_playlist(type,channel,sid,h) {
+    if(!auth_obj.cookie) return null;
+    var data={
+                'type':(type||'n'),
+                'channel':0,
+                'r':Math.round(Math.random()*RANGE_R+MIN_R).toString(16),
+            }
+    if(sid) data['sid']=sid;
+    if(h) data['h']=h;
+    $.ajax('http://douban.fm/j/mine/playlist',
+            {
+                'data':data,
+                'type':'get',
+                'beforeSend':function(xhr){
+                    xhr.setRequestHeader('Cookie',auth_obj.cookie)
+                },
+                'success':function(json,xhr)
+                {
+                    var list=eval(json);
+                    if(list.r) throw(2,'Get Playlist Failed Error');
+                    if(data.type=='n')
+                        playlist=list.song;
+                    //remember to fire a new event
+                },
+            });
+}
+
 
 
 function error_handle(err) {
