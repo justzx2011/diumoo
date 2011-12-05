ba5de2b9cdb61b81d45fbe0173b9d273320cd06f
shanzi <yun.er.run@gmail.com>
2011-12-05 22:00:28 +0800

    init

---
+++ diumoo/diumoo/diumooAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  diumooAppDelegate.h
+//  diumoo
+//
+//  Created by Shanzi on 11-12-5.
+//  Copyright 2011年 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface diumooAppDelegate : NSObject <NSApplicationDelegate> {
+    NSWindow *window;
+}
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ diumoo/diumoo/diumooAppDelegate.m
@@ -0,0 +1,20 @@
+//
+//  diumooAppDelegate.m
+//  diumoo
+//
+//  Created by Shanzi on 11-12-5.
+//  Copyright 2011年 __MyCompanyName__. All rights reserved.
+//
+
+#import "diumooAppDelegate.h"
+
+@implementation diumooAppDelegate
+
+@synthesize window;
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
