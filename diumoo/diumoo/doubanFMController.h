f8d83456cb1e675d04d25d60a0bc22e9084b04a8
shanzi <yun.er.run@gmail.com>
2011-12-06 13:50:02 +0800

    init douban fm controller

---
+++ diumoo/diumoo/doubanFMController.h
@@ -0,0 +1,13 @@
+//
+//  doubanFMController.h
+//  diumoo
+//
+//  Created by Shanzi on 11-12-6.
+//  Copyright 2011年 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface doubanFMController : NSObject
+
+@end
---
+++ diumoo/diumoo/doubanFMController.m
@@ -0,0 +1,23 @@
+//
+//  doubanFMController.m
+//  diumoo
+//
+//  Created by Shanzi on 11-12-6.
+//  Copyright 2011年 __MyCompanyName__. All rights reserved.
+//
+
+#import "doubanFMController.h"
+
+@implementation doubanFMController
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
