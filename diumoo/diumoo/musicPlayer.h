7ad29f333dd402f1176f7d5b9550f0d5863ac36b
shanzi <yun.er.run@gmail.com>
2011-12-08 23:46:32 +0800

    将Source 重命名为TouchJSON

---
+++ diumoo/diumoo/musicPlayer.h
@@ -0,0 +1,13 @@
+//
+//  remoteMusicSourcePlayer.h
+//  diumoo
+//
+//  Created by Shanzi on 11-12-8.
+//  Copyright 2011年 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface remoteMusicSourcePlayer : NSObject
+
+@end
---
+++ diumoo/diumoo/musicPlayer.m
@@ -0,0 +1,23 @@
+//
+//  remoteMusicSourcePlayer.m
+//  diumoo
+//
+//  Created by Shanzi on 11-12-8.
+//  Copyright 2011年 __MyCompanyName__. All rights reserved.
+//
+
+#import "remoteMusicSourcePlayer.h"
+
+@implementation remoteMusicSourcePlayer
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
