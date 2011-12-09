efa11ddec9f200b8b4eb152d8bdef889e4644264
shanzi <yun.er.run@gmail.com>
2011-12-10 01:15:00 +0800

    添加musicController 测试完成播放暂停和切换电台功能，rate unrate功能尚未测试

---
+++ diumoo/diumoo/musicController.h
@@ -0,0 +1,13 @@
+//
+//  musicController.h
+//  diumoo
+//
+//  Created by Shanzi on 11-12-9.
+//  Copyright 2011年 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface musicController : NSObject
+
+@end
---
+++ diumoo/diumoo/musicController.m
@@ -0,0 +1,23 @@
+//
+//  musicController.m
+//  diumoo
+//
+//  Created by Shanzi on 11-12-9.
+//  Copyright 2011年 __MyCompanyName__. All rights reserved.
+//
+
+#import "musicController.h"
+
+@implementation musicController
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
