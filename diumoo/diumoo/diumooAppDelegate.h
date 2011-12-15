549937f348a21715de020e4b4e9806649aa77878
shanzi <yun.er.run@gmail.com>
2011-12-15 21:16:20 +0800

    recover diumoappdelegate and main

---
+++ diumoo/diumoo/diumooAppDelegate.h
@@ -0,0 +1,28 @@
+//
+//  diumooAppDelegate.h
+//  diumoo
+//
+//  Created by Shanzi on 11-12-5.
+//  Copyright 2011年 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#import "doubanFMSource.h"
+#import "musicPlayer.h"
+#import "musicController.h"
+#import "growlNotifier.h"
+#import "menu.h"
+
+@interface diumooAppDelegate : NSObject <NSApplicationDelegate> {
+    //NSWindow *window;
+    musicPlayer* p;
+    doubanFMSource* s;
+    musicController* c;
+    growlNotifier* g;
+    menu* m;
+}
+
+-(void) backinit;
+-(void) applicationWillTerminate:(NSNotification *)notification;
+
+@end
---
+++ diumoo/diumoo/diumooAppDelegate.m
@@ -0,0 +1,38 @@
+//
+//  diumooAppDelegate.m
+//  diumoo
+//
+//  Created by Shanzi on 11-12-5.
+//  Copyright 2011年 __MyCompanyName__. All rights reserved.
+//
+
+#import "diumooAppDelegate.h"
+
+@implementation diumooAppDelegate
+
+//@synthesize window;
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+    g=[[[growlNotifier alloc] init] retain];
+    s=[[[doubanFMSource alloc] init] retain];
+    p=[[[musicPlayer alloc] init] retain];
+    c=[[[musicController alloc] init] retain];
+    m=[[[menu alloc]init] retain];
+    [m setController:c];
+    [self performSelectorInBackground:@selector(backinit) withObject:nil];
+}
+
+-(void)backinit
+{
+    [c setSource:s];
+    [c setPlayer:p];
+}
+
+-(void) applicationWillTerminate:(NSNotification *)notification
+{
+    [c pause];
+}
+
+@end
